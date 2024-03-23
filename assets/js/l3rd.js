c4fb2196106b552e3308b6138e50f6c3e55d41d7
Gregory Troinenko <grintroy@gmail.com>
2024-03-23 23:14:41 +0000

    Update for show

--- assets/js/l3rd.js
+++ assets/js/l3rd.js
@@ -2,20 +2,35 @@ webcg.on("data", function (data) {
 	document.querySelector("#primary").innerHTML = data.primary
 		? data.primary.text || data.primary
 		: "";
-	document.querySelector("#secondary").innerHTML = data.secondary
-		? data.secondary.text || data.secondary
-		: "";
-	document.querySelector("#separator").innerHTML = data.separator
-		? data.separator.text || data.separator
-		: "";
+
+	const secondary = document.querySelector("#secondary")
+	if (!data.secondary) {
+		secondary.hidden = true
+	} else {
+		secondary.hidden = false
+		secondary.innerHTML = data.secondary;
+	}
+	
+	const separator = document.querySelector("#separator")
+	if (!data.separator) {
+		separator.hidden = true
+	} else {
+		separator.hidden = false
+		separator.innerHTML = data.separator;
+	}
+
+	const fontSize = data.fontSize
+	? data.fontSize.text || data.fontSize
+	: "26";
+	document.querySelector(".votesBoxFace").style.fontSize = `${fontSize}pt`
 });
 
 webcg.on("play", () => {
 	document.querySelector("#l3rdBox").classList.add("active");
-	document.querySelector("video").play();
+	document.querySelector(".backgroundVid").play();
 });
 
 webcg.on("stop", () => {
 	document.querySelector("#l3rdBox").classList.remove("active");
-	document.querySelector("video").pause();
+	document.querySelector(".backgroundVid").pause();
 });
